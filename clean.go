48b9919e7177b13df84e6c37bfa58d8deda7b240
Admin <2762713521@qq.com>
2025-06-18 20:23:30 +0800

    dev

--- clean.go
+++ clean.go
@@ -7,65 +7,88 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
 type (
-	info struct {
-		include string
-		lib     string
-		bin     string
-	}
 	setup struct {
 		findDiaSdk         func()
 		findMsvcBuildTools func()
 		findWdk            func()
 	}
 )
+type bin struct {
+	cc   string
+	lib  string
+	link string
+	asm  string
+}
+type Config struct {
+	userIncludes []string
+	user64Libs   []string
+	user32Libs   []string
 
-func Walk() {
-	//var (
-	//	user64Includes  []string
-	//	user64Libs      []string
-	//	user64Bins      []string
-	//
-	//	user32Includes  []string
-	//	user32Libs      []string
-	//	user32Bins      []string
-	//
-	//	kernel64Includes  []string
-	//	kernel64Libs      []string
-	//	kernel64Bins      []string
-	//
-	//	kernel32Includes  []string
-	//	kernel32Libs      []string
-	//	kernel32Bins      []string
-	//)
+	kernelIncludes []string
+	kernel64Libs   []string
+	kernel32Libs   []string
 
+	Bins64   bin
+	Bins32   bin
+	msdia140 string
 }
 
 func main() {
+	Walk()
+}
+func Walk() Config {
+	cfg := Config{
+		userIncludes:   make([]string, 0),
+		user64Libs:     make([]string, 0),
+		user32Libs:     make([]string, 0),
+		kernelIncludes: make([]string, 0),
+		kernel64Libs:   make([]string, 0),
+		kernel32Libs:   make([]string, 0),
+		Bins64: bin{
+			cc:   "",
+			lib:  "",
+			link: "",
+			asm:  "",
+		},
+		Bins32: bin{
+			cc:   "",
+			lib:  "",
+			link: "",
+			asm:  "",
+		},
+		msdia140: "",
+	}
+
 	//stream.UpdateAllLocalRep()
 	root := "V:"
 	if stream.IsRunningOnGitHubActions() {
 		root = "/mnt/ewdk"
 	}
 	mylog.Success("root: ", root)
-	//const tmp = "tmp"
-	const tmp = "ewdk"
-	os.RemoveAll(tmp)
+	outDir := "D:/ewdk/dist"
+	if stream.IsRunningOnGitHubActions() {
+		outDir = "dist"
+	}
+	os.RemoveAll(outDir)
 	BuildTools := filepath.Join(root, "Program Files", "Microsoft Visual Studio", "2022", "BuildTools")
 
 	fnFixPath := func(path string) string {
 		fixPath := strings.TrimPrefix(path, root)
 		fixPath = strings.ReplaceAll(fixPath, " ", "_")
-		fixPath = filepath.Join(tmp, fixPath)
+		fixPath = filepath.Join(outDir, fixPath)
 		fixPath = filepath.ToSlash(fixPath)
 		return fixPath
 	}
 
 	s := setup{
 		findDiaSdk: func() {
-			filepath.Walk(filepath.Join(BuildTools, "DIA SDK"), func(path string, info fs.FileInfo, err error) error {
+			once := sync.Once{}
+			diaRoot := filepath.Join(BuildTools, "DIA SDK")
+			filepath.Walk(diaRoot, func(path string, info fs.FileInfo, err error) error {
 				if strings.Contains(path, "arm") {
 					return nil
 				}
@@ -74,8 +97,19 @@ func main() {
 						return nil
 					}
 				}
-				fixPath := fnFixPath(path)
-				stream.CopyFile(path, fixPath)
+
+				switch filepath.Ext(path) {
+				case ".dll", ".lib":
+					if !strings.Contains(path, "amd64") {
+						return nil
+					}
+				}
+				once.Do(func() {
+					cfg.msdia140 = fnFixPath(filepath.Join(diaRoot, "bin", "amd64", "msdia140.dll"))
+					cfg.userIncludes = append(cfg.userIncludes, fnFixPath(filepath.Join(diaRoot, "include")))
+					cfg.user64Libs = append(cfg.user64Libs, fnFixPath(filepath.Join(diaRoot, "lib", "amd64")))
+				})
+				stream.CopyFile(path, fnFixPath(path))
 				return err
 			})
 		},
@@ -89,32 +123,52 @@ func main() {
 				return err
 			})
 
-			msvc64 := info{
-				include: filepath.Join(msvc, "include"),
-				lib:     filepath.Join(msvc, "lib", "x64"),
-				bin:     filepath.Join(msvc, "bin", "Hostx64", "x64"),
+			include := filepath.Join(msvc, "include")
+			fixInclude := fnFixPath(include)
+			stream.CopyDir(include, fixInclude)
+
+			bin64Root := filepath.Join(msvc, "bin", "Hostx64", "x64")
+			lib64 := filepath.Join(msvc, "lib", "x64")
+			stream.CopyDir(lib64, fnFixPath(lib64))
+
+			bin32Root := filepath.Join(msvc, "bin", "Hostx64", "x86")
+			lib32 := filepath.Join(msvc, "lib", "x86")
+			stream.CopyDir(lib32, fnFixPath(lib32))
+
+			stream.CopyDir(bin64Root, fnFixPath(bin64Root))
+			stream.CopyDir(bin32Root, fnFixPath(bin32Root))
+
+			Bins64 := bin{
+				cc:   filepath.Join(bin64Root, "cl.exe"),
+				lib:  filepath.Join(lib64, "lib.exe"),
+				link: filepath.Join(bin64Root, "link.exe"),
+				asm:  filepath.Join(bin64Root, "ml64.exe"),
 			}
-			msvc32 := info{
-				include: filepath.Join(msvc, "include"),
-				lib:     filepath.Join(msvc, "lib", "x86"),
-				bin:     filepath.Join(msvc, "bin", "Hostx64", "x86"),
+			Bins32 := bin{
+				cc:   filepath.Join(bin32Root, "cl.exe"),
+				lib:  filepath.Join(lib32, "lib.exe"),
+				link: filepath.Join(bin32Root, "link.exe"),
+				asm:  filepath.Join(bin32Root, "ml.exe"),
 			}
-			fixMsvc64 := info{
-				include: fnFixPath(msvc64.include),
-				lib:     fnFixPath(msvc64.lib),
-				bin:     fnFixPath(msvc64.bin),
+
+			fixBin64 := bin{
+				cc:   fnFixPath(Bins64.cc),
+				lib:  fnFixPath(Bins64.lib),
+				link: fnFixPath(Bins64.link),
+				asm:  fnFixPath(Bins64.asm),
 			}
-			fixMsvc32 := info{
-				include: fnFixPath(msvc32.include),
-				lib:     fnFixPath(msvc32.lib),
-				bin:     fnFixPath(msvc32.bin),
+			fixBin32 := bin{
+				cc:   fnFixPath(Bins32.cc),
+				lib:  fnFixPath(Bins32.lib),
+				link: fnFixPath(Bins32.link),
+				asm:  fnFixPath(Bins32.asm),
 			}
-			stream.CopyDir(msvc64.include, fixMsvc64.include)
-			stream.CopyDir(msvc64.lib, fixMsvc64.lib)
-			stream.CopyDir(msvc64.bin, fixMsvc64.bin)
-			//stream.CopyDir(msvc32.include, fixMsvc32.include)
-			stream.CopyDir(msvc32.lib, fixMsvc32.lib)
-			stream.CopyDir(msvc32.bin, fixMsvc32.bin)
+			cfg.Bins64 = fixBin64
+			cfg.Bins32 = fixBin32
+
+			cfg.userIncludes = append(cfg.userIncludes, fnFixPath(include))
+			cfg.user64Libs = append(cfg.user64Libs, fnFixPath(lib64))
+			cfg.user32Libs = append(cfg.user32Libs, fnFixPath(lib32))
 		},
 		findWdk: func() {
 			// V:Program Files\Windows Kits\10\Include\10.0.26100.0\km\ntddk.h
@@ -138,33 +192,21 @@ func main() {
 				return err
 			})
 
-			wdk64 := info{
-				include: filepath.Join(wdkRoot, "Include"), //linux 对大小写敏感，todo x64盒x86合并了，需要区分
-				lib:     filepath.Join(wdkRoot, "Lib"),
-			}
-			//wdk32 := info{
-			//	include: filepath.Join(wdkRoot, "include"),
-			//	lib:     filepath.Join(wdkRoot, "lib"),
-			//}
-			fixMsvc64 := info{
-				include: fnFixPath(wdk64.include),
-				lib:     fnFixPath(wdk64.lib),
-			}
-			//fixWdk32 := info{
-			//	include: fnFixPath(wdk32.include),
-			//	lib:     fnFixPath(wdk32.lib),
-			//}
+			include := filepath.Join(wdkRoot, "Include") //linux 对大小写敏感，to
+			lib := filepath.Join(wdkRoot, "Lib")
 
-			mylog.Success(wdk64.include, fixMsvc64.include)
-			mylog.Success(wdk64.lib, fixMsvc64.lib)
+			stream.CopyDir(include, fnFixPath(include))
+			stream.CopyDir(lib, fnFixPath(lib))
 
-			stream.CopyDir(wdk64.include, fixMsvc64.include)
-			stream.CopyDir(wdk64.lib, fixMsvc64.lib)
-			//stream.CopyDir(wdk32.include, fixWdk32.include)
-			//stream.CopyDir(wdk32.lib, fixWdk32.lib)
+			cfg.kernelIncludes = append(cfg.kernelIncludes, fnFixPath(include)) //todo bug
+			cfg.kernel64Libs = append(cfg.kernel64Libs, fnFixPath(lib))
+			cfg.kernel32Libs = append(cfg.kernel32Libs, fnFixPath(lib))
+
+			mylog.Struct(cfg)
+			return
 
 			filepath.Walk(wdkRoot, func(path string, info fs.FileInfo, err error) error {
-				mylog.Info(path)
+				//mylog.Info(path)
 				//if filepath.Base(path) == "Include" {
 				//	println(path)
 				//}
@@ -175,4 +217,5 @@ func main() {
 	s.findDiaSdk()
 	s.findMsvcBuildTools()
 	s.findWdk()
+	return cfg
 }
