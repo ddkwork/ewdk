9e53d2116e040bfe96853af4f9cdd618d600f0be
ddkwork <2762713521@qq.com>
2026-06-25 09:03:22 +0800

    build: 更新构建脚本和gitignore规则
    1. 调整build.bat中的构建输出目录为Release和Debug
    2. 新增多种日志、IDE、构建产物目录到gitignore

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release . && cmake --build build --config Release
-cmake -B build_debug -G "Ninja" -DCMAKE_BUILD_TYPE=Debug . && cmake --build build_debug --config Debug
+cmake -B Release -G "Ninja" -DCMAKE_BUILD_TYPE=Release . && cmake --build Release --config Release
+cmake -B Debug -G "Ninja" -DCMAKE_BUILD_TYPE=Debug . && cmake --build Debug --config Debug
